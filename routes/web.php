d4c5529efe0b9491de867a162f887ae75a2344e8
ElimuStuart <stuartelimu@gmail.com>
2019-08-16 06:10:05 +0300

    added routes for the profile controller

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,6 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/profile', 'ProfileController@index')->name('profile');
+Route::post('/profile/update', 'ProfileController@updateProfile')->name('profile.update');
